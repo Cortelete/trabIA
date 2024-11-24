8c6a70c9c5731ca5c5800aa4941cf61dc190958f
Cortelete <d.cortelete@gmail.com>
2024-11-24 02:30:14 -0300

    será que vai?

--- TrabalhoFF/MotherBrain/src/main/java/com/mycompany/motherbrain/Neuron.java
+++ TrabalhoFF/MotherBrain/src/main/java/com/mycompany/motherbrain/Neuron.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.Objects;
 import org.decimal4j.util.DoubleRounder;
 
-/**
- *
- * @author will
- */
 public class Neuron {
 
     private InputLayer inputLayer;
@@ -20,11 +16,10 @@ public class Neuron {
     private ArrayList<Double> y;
     private int bias;
 
-    // Construtor
     public Neuron(InputLayer il, double learningRate, double minimumAcuracy, ArrayList<Double> y, String activationFunction, int bias) {
         this.inputLayer = il;
         this.learningRate = learningRate;
-        this.weight = new ArrayList<>();
+        this.weight = new ArrayList<Double>();
         this.weightInitialization();
         this.activationInitialization(activationFunction);
         this.minimumAcuracy = minimumAcuracy;
@@ -32,92 +27,101 @@ public class Neuron {
         this.bias = bias;
     }
 
-    // Inicializa os pesos com valor 1.0
     private void weightInitialization() {
         for (Object il : this.inputLayer.getInputs()) {
             this.weight.add(1.0);
         }
     }
 
-    // Treinamento do neurônio
     public double Training() {
-        double accuracy = 0.0;
-        ArrayList<Double> outputs;
+        double acuracy = 0.0;
+        ArrayList<Double> outPuts;
 
-        int iteration = 0; // Contador de iterações
+        int iteration = 0;
+
         while (true) {
             iteration++;
-            System.out.println("Início da iteração: " + iteration);
+            outPuts = new ArrayList<>();
 
-            outputs = new ArrayList<>();
+            // Calcula a saída para cada amostra de entrada
             for (int i = 0; i < this.inputLayer.getInputs().get(0).getInput().size(); i++) {
                 ArrayList<Double> row = new ArrayList<>();
                 for (int j = 0; j < this.inputLayer.getInputs().size(); j++) {
                     row.add(this.inputLayer.getInputs().get(j).getInput().get(i));
                 }
 
+                // Calcula a soma ponderada e a saída da função de ativação
                 double sum = this.sumFunction(row);
                 double output = this.function.output(sum);
-                outputs.add(output);
+
+                // Adiciona a saída calculada à lista
+                outPuts.add(output);
             }
 
-            // Calcula a precisão
-            accuracy = this.calculateAccuracy(outputs);
-            System.out.println("Precisão atual: " + accuracy + "%");
+            // Calcula a acurácia
+            acuracy = this.calculateAcuracy(outPuts);
 
-            if (accuracy >= this.minimumAcuracy) {
-                System.out.println("Treinamento concluído após " + iteration + " iterações!");
+            System.out.println("Iteração: " + iteration);
+            System.out.println("Acurácia atual: " + acuracy + "%");
+
+            // Condição de parada
+            if (acuracy >= this.minimumAcuracy) {
+                System.out.println("Treinamento concluído com precisão de " + acuracy + "%");
                 return DoubleRounder.round(this.weight.get(0), 3);
             }
 
-            // Atualiza os pesos
-            double totalError = 0.0;
-            for (int i = 0; i < outputs.size(); i++) {
-                double output = outputs.get(i);
+            // Calcula o erro médio
+            double totalError = 0;
+            for (int i = 0; i < outPuts.size(); i++) {
+                double output = outPuts.get(i);
                 double target = this.y.get(i);
-                totalError += target - output;
+                totalError += target - output; // Acumula os erros
             }
+            double meanError = totalError / outPuts.size(); // Calcula o erro médio
 
-            double meanError = totalError / outputs.size();
             System.out.println("Erro médio: " + meanError);
 
+            // Atualiza os pesos
             for (int j = 0; j < this.weight.size(); j++) {
-                double inputSum = 0.0;
-                for (int i = 0; i < outputs.size(); i++) {
+                double inputSum = 0;
+
+                // Calcula a média dos inputs para este peso específico
+                for (int i = 0; i < outPuts.size(); i++) {
                     double input = this.inputLayer.getInputs().get(j).getInput().get(i);
                     inputSum += input;
                 }
+                double meanInput = inputSum / outPuts.size();
 
-                double meanInput = inputSum / outputs.size();
+                // Verifica se há valores válidos antes de atualizar os pesos
+                if (Double.isNaN(meanInput) || Double.isNaN(meanError) || Double.isNaN(this.weight.get(j))) {
+                    System.err.println("Erro: valor NaN detectado. Debug:");
+                    System.err.println("Input Médio: " + meanInput + ", Erro Médio: " + meanError + ", Peso Atual: " + this.weight.get(j));
+                    return Double.MIN_VALUE;
+                }
+
+                // Atualiza o peso
                 double newWeight = this.weight.get(j) + (this.learningRate * meanInput * meanError);
-                System.out.println("Peso atualizado [" + j + "]: " + newWeight);
                 this.weight.set(j, newWeight);
             }
-
-            // Evitar loops infinitos para debug inicial
-            if (iteration > 10000) {
-                System.err.println("Treinamento interrompido por excesso de iterações.");
-                break;
-            }
         }
-
-        return -1.0; // Retorno em caso de falha
     }
 
-    // Função de soma
     private double sumFunction(ArrayList<Double> inputs) {
-        double output = 0.0;
+        double outPut = 0;
         for (int i = 0; i < inputs.size(); i++) {
-            output += this.bias + (inputs.get(i) * this.weight.get(i));
+            outPut += this.bias + (inputs.get(i) * this.weight.get(i));
         }
-        return output;
+        return outPut;
     }
 
-    // Inicializa a função de ativação
     private void activationInitialization(String activationFunction) {
         switch (activationFunction) {
-            case LINEAR -> this.function = new LinearFunction();
-            case STEP -> this.function = new StepFunction();
+            case LINEAR -> {
+                this.function = new LinearFunction();
+            }
+            case STEP -> {
+                this.function = new StepFunction();
+            }
             default -> {
                 System.out.println("Por favor, selecione uma função de ativação válida.");
                 System.exit(0);
@@ -125,17 +129,18 @@ public class Neuron {
         }
     }
 
-    // Calcula a precisão
-    private double calculateAccuracy(ArrayList<Double> outputs) {
-        double correctPredictions = 0.0;
-        for (int i = 0; i < outputs.size(); i++) {
+    private double calculateAcuracy(ArrayList<Double> outPuts) {
+        double accuracy = 0.0;
+        for (int i = 0; i < outPuts.size(); i++) {
+            // Arredonda ambos os valores para 4 casas decimais
             double targetRounded = DoubleRounder.round(this.y.get(i), 4);
-            double outputRounded = DoubleRounder.round(outputs.get(i), 4);
+            double outputRounded = DoubleRounder.round(outPuts.get(i), 4);
 
+            // Incrementa a contagem de acertos se forem iguais
             if (Objects.equals(targetRounded, outputRounded)) {
-                correctPredictions++;
+                accuracy++;
             }
         }
-        return (correctPredictions / outputs.size()) * 100;
+        return (accuracy / outPuts.size()) * 100;
     }
 }
